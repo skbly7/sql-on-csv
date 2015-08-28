9ffd91673e13c066b97957f6ba3f2e042664756e
Shivam Khandelwal <skbly7@gmail.com>
2015-08-28 19:56:33 +0530

    -> Added shell type environment, and argv runing option -> Error message (referring to MySQL and basic code structure -> Added exit functionality

--- run.py
+++ run.py
@@ -1,5 +1,58 @@
-import sqlparse as sql
+import sqlparse
+import sys
 
+class Dbms:
+    def __init__(self):
+        self.__query = []
+        self.__input = ''
+        pass
+
+    def __error_message__(self, code):
+        error_msg_dict = {
+            0 : "Thanks for using skbly7's DBMS. Bye.",
+            1064 : "ERROR 1064 (42000): You have an error in your SQL syntax; "
+                   "check the manual for the right syntax to use."
+        }
+        return error_msg_dict[code]
+
+
+    def __break_n_execute_query__(self):
+        for query in self.__query:
+            self.__execute__(query)
+
+    def __type_verify_wrapper__(self, parsed_query):
+        type = parsed_query.get_type()
+        verified = True
+        if type == u'UNKNOWN':
+            if str.upper(str(parsed_query.tokens[0])) == u'EXIT':
+                print self.__error_message__(0)
+                exit(0)
+            else:
+                print self.__error_message__(1064)
+                verified = False
+        return verified
+
+    def __execute__(self, query):
+        parsed_query = sqlparse.parse(query)[0]
+        type_verified = self.__type_verify_wrapper__(parsed_query)
+        if type_verified:
+            print 'Execute query : ', query
+
+    def execute(self, input):
+        self.__input = input
+        self.__query = sqlparse.split(self.__input)
+        self.__break_n_execute_query__()
+
+
+def init_shell(cursor):
+    while 1:
+        query = raw_input(">> ")
+        cursor.execute(query)
+    pass
 
 if __name__ == '__main__':
-    print "Hello world !"
+    cursor = Dbms()
+    if len(sys.argv) > 1:
+        cursor.execute(sys.argv[1])
+    else:
+        init_shell(cursor)
